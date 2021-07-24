30cc79a0ebb36843ffed437279372df8aff06ee1
TakKoubu <kokubu.tk@gmail.com>
2021-07-24 10:23:25 +0000

    猫犬用画像の調整

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@
 
 // モバイルブラウザかどうか判定
 const isMobile = !!new MobileDetect(window.navigator.userAgent).mobile();
-const apiKey = "78a99c805792251c05fe432a05591e2f";
 /**
  * ----------------------
  * 指定された名前のタブを表示
@@ -207,3 +206,169 @@ showTab('kittens-1');
 
 // パララックスを初期化する
 initParallax();
+
+
+// Flickr画像データのURLを返す
+const getFlickrImageURL = (photo, size) => {
+  let url = `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${
+    photo.secret
+  }`;
+  if (size) {
+    // サイズ指定ありの場合
+    url += `_${size}`;
+  }
+  url += '.jpg';
+  return url;
+};
+
+// Flickr画像の元ページのURLを返す
+const getFlickrPageURL = photo => `https://www.flickr.com/photos/${photo.owner}/${photo.id}`;
+
+// Flickr画像のaltテキストを返す
+const getFlickrText = (photo) => {
+  let text = `"${photo.title}" by ${photo.ownername}`;
+  if (photo.license === '4') {
+    // Creative Commons Attribution（CC BY）ライセンス
+    text += ' / CC BY';
+  }
+  return text;
+};
+
+// Flickr API key
+const apiKey = '78a99c805792251c05fe432a05591e2f';
+
+// リクエストパラメータを作る
+const parameterscat = $.param({
+  method: 'flickr.photos.search',
+  api_key: apiKey,
+  text: 'cat', // 検索テキスト
+  sort: 'interestingness-desc', // 興味深さ順
+  per_page: 4, // 取得件数
+  license: '4', // Creative Commons Attributionのみ
+  extras: 'owner_name,license', // 追加で取得する情報
+  format: 'json', // レスポンスをJSON形式に
+  nojsoncallback: 1, // レスポンスの先頭に関数呼び出しを含めない
+});
+
+const parametersdog = $.param({
+  method: 'flickr.photos.search',
+  api_key: apiKey,
+  text: 'dog', // 検索テキスト
+  sort: 'interestingness-desc', // 興味深さ順
+  per_page: 4, // 取得件数
+  license: '4', // Creative Commons Attributionのみ
+  extras: 'owner_name,license', // 追加で取得する情報
+  format: 'json', // レスポンスをJSON形式に
+  nojsoncallback: 1, // レスポンスの先頭に関数呼び出しを含めない
+});
+
+const caturl = `https://api.flickr.com/services/rest/?${parameterscat}`;
+const dogurl = `https://api.flickr.com/services/rest/?${parametersdog}`;
+
+console.log(caturl);
+console.log(dogurl);
+
+// 猫の画像を検索して表示
+$.getJSON(caturl, (data) => {
+  console.log(data);
+
+  // データが取得できなかった場合
+  if (data.stat !== 'ok') {
+    console.error('データの取得に失敗しました。');
+    return;
+  }
+  // 空の<div>を作る
+  const $div = $('<div>');
+
+  // ヒット件数
+  // $div.append(`<div>${data.photos.total} photos in total</div>`);
+
+  for (let i = 0; i < data.photos.photo.length; i++) {
+    const photo = data.photos.photo[i];
+    const photoText = getFlickrText(photo);
+
+    // $divに <a href="..." ...><img src="..." ...></a> を追加する
+    $div.append(
+      $('<div>', {
+        class: 'image-gallery__item',
+        'rel': "noopener noreferrer",
+        'data-toggle': 'tooltip',
+        'data-placement': 'bottom',
+      }).append(
+        $('<a>', {
+          href: getFlickrPageURL(photo),
+          class: 'd-inline-block',
+          target: '_blank', // リンクを新規タブで開く
+          'rel': "noopener noreferrer",
+          'data-toggle': 'tooltip',
+          'data-placement': 'bottom',
+          title: photoText,
+        }).append(
+          $('<img>', {
+            src: getFlickrImageURL(photo, 'q'),
+            width: 150,
+            height: 150,
+            alt: photoText,
+          }),
+        ),
+      ),
+    );
+  }
+  // $divを#mainに追加する
+  $div.appendTo('.cat_pic');
+
+  // BootstrapのTooltipを適用
+  $('[data-toggle="tooltip"]').tooltip();
+});
+
+$.getJSON(dogurl, (data) => {
+  console.log(data);
+
+  // データが取得できなかった場合
+  if (data.stat !== 'ok') {
+    console.error('データの取得に失敗しました。');
+    return;
+  }
+  // 空の<div>を作る
+  const $div = $('<div>');
+
+  // ヒット件数
+  // $div.append(`<div>${data.photos.total} photos in total</div>`);
+
+  for (let i = 0; i < data.photos.photo.length; i++) {
+    const photo = data.photos.photo[i];
+    const photoText = getFlickrText(photo);
+
+    // $divに <a href="..." ...><img src="..." ...></a> を追加する
+    $div.append(
+      $('<div>', {
+        class: 'image-gallery__item',
+        'rel': "noopener noreferrer",
+        'data-toggle': 'tooltip',
+        'data-placement': 'bottom',
+      }).append(
+        $('<a>', {
+          href: getFlickrPageURL(photo),
+          class: 'd-inline-block',
+          target: '_blank', // リンクを新規タブで開く
+          'rel': "noopener noreferrer",
+          'data-toggle': 'tooltip',
+          'data-placement': 'bottom',
+          title: photoText,
+        }).append(
+          $('<img>', {
+            src: getFlickrImageURL(photo, 'q'),
+            width: 150,
+            height: 150,
+            alt: photoText,
+          }),
+        ),
+      ),
+    );
+  }
+  // $divを#mainに追加する
+  $div.appendTo('.dog_pic');
+
+  // BootstrapのTooltipを適用
+  $('[data-toggle="tooltip"]').tooltip();
+});
